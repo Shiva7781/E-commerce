b5f886e92a1bfe6324d39f9fc5e35f2f4f4aad0b
Shiva7781 <shivavishwakarma7781@gmail.com>
2022-11-15 16:29:12 +0530

    Nav, Slider, Cat, Pro

---
+++ frontend/src/components/Announcement.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return (
+    <Container>
+      Super Deal!
+      {/* <span role="img" aria-label="face with open mouth">
+        😮
+      </span> */}
+      Live Now
+    </Container>
+  );
+};
+
+export default Announcement;
