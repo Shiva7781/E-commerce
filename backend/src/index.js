e2d421a5b285613b2e7c53241c36a0108c18ab00
Shiva7781 <shivavishwakarma7781@gmail.com>
2022-11-21 14:19:25 +0530

    Stripe Payment Added

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,10 @@ const authRoute = require("./routes/auth.controller");
 const productRoute = require("./routes/product.controller");
 const cartRoute = require("./routes/cart.controller");
 const orderRoute = require("./routes/order.controller");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -14,5 +17,6 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 module.exports = app;
---
+++ backend/src/routes/stripe.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const KEY = process.env.STRIPE_KEY;
+const stripe = require("stripe")(KEY);
+
+router.post("/payment", (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "INR",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
+
+module.exports = router;
